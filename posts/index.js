e35d914969c6445b60b4dd42ad7a1cc453acb97b
chandan yadav <chandanky.23@gmail.com>
2020-11-05 01:25:52 +0100

    Re-Setting up the project

--- posts/index.js
+++ posts/index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const { randomBytes } = require('crypto')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// local posts data storage (no database implementation)
-const posts = {}
+const posts = {};
 
 app.get('/posts', (req, res) => {
-  res.send(posts)
-} )
+  res.send(posts);
+});
 
 app.post('/posts', (req, res) => {
-  const id = randomBytes(4).toString('hex')
-  const { title } = req.body
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
   posts[id] = {
     id,
     title
-  }
+  };
 
-  res.status(201).send(posts[id])
-})
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
-  console.log('Listening on port: 4000')
-})
+  console.log('Listening on 4000');
+});
