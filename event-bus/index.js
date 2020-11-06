c0559cebfd7f41c0318a579f21b6ec4cb0c8f478
chandan yadav <chandanky.23@gmail.com>
2020-11-06 01:07:55 +0100

    Adding an in memory data store in the Query service to handle case where it may have gone down and missed certain events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios")
 const app = express()
 app.use(bodyParser.json())
 
+const event_store = []
+
 app.post("/events", (req, res) => {
   const event = req.body
+
+  event_store.push(event)
 
   axios.post("http://localhost:4000/events", event)
   axios.post("http://localhost:4001/events", event)
@@ -16,6 +20,10 @@ app.post("/events", (req, res) => {
   res.send({
     status: "OK",
   })
+})
+
+app.get('/events', (req, res) => {
+  res.send(event_store)
 })
 
 app.listen(4005, () => {
